1ee9753f43797ef345ed600b43435d2a9946e6de
blakley <blakley.ent@gmail.com>
2021-04-15 22:15:50 -0400

    added more game assets created more class structures changed game resolution

--- scripts/main.py
+++ scripts/main.py
@@ -18,29 +18,76 @@ class Player():
 
 #
 class World():
-	def __init__(self, data):
+	def __init__(self, screen):
 		self.tiles = []
+		self.load_assets()
+		self.screen = screen
+		self.setup_data()
+
+	# loads all the game assets
+	def load_assets(self):
+		# load then update the background scale
+		self.bg1 = pygame.image.load(settings.imgT[0])
+		self.bg1 = pygame.transform.scale(self.bg1, (settings.w, settings.h)) 
+		self.sun = pygame.image.load(settings.imgT[1]) # load sun
+		self.dirt = pygame.image.load(settings.imgT[4]) # load dirt
+
+
+	# setup images and tile data
+	def setup_data(self):
+		row = 0
+		for r in settings.data:
+			col = 0
+			for tile in r:
+				if (tile == 1): # grass_tile ID
+					img = pygame.transform.scale(self.dirt, (settings.tile_size, settings.tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col * settings.tile_size
+					img_rect.y = row * settings.tile_size
+					tile = (img, img_rect)
+					self.tiles.append(tile)
+				col += 1
+			row += 1
+
+	# Test grid
+	def draw_grid(self):
+		for line in range(0, 20):
+			pygame.draw.line(self.screen, (255, 255, 255), (0, line * settings.tile_size), (settings.w, line * settings.tile_size))
+			pygame.draw.line(self.screen, (255, 255, 255), (line * settings.tile_size, 0), (line * settings.tile_size, settings.h))
+
+	# draw the world assets
+	def draw_world(self):
+		self.screen.blit(self.bg1, (0, 0))
+		self.screen.blit(self.sun, (100, 100))
 
 	# draws the world data from the tiles we created
 	def draw(self):
 		for tile in self.tiles:
-			screen.blit(tile[0], tile[1]) # draw the (img, img_rect)
+			self.screen.blit(tile[0], tile[1]) # draw the (img, img_rect)
 
 #
 class Game():
 	def __init__(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((settings.w, settings.h))
-		pygame.display.set_caption('Mazer')
+		pygame.display.set_caption('Mazer') # title
+		# icon = pygame.image.load('../assets/icon.png') # icon
+		# pygame.display.set_icon(icon)
 		self.start()
 
 	def start(self):
+		world = World(self.screen)
 		run = True 
 		while(run):
+			# draw game content
+			world.draw_world()
+			world.draw_grid()
+			world.draw()
+			
 			for event in pygame.event.get(): 
 				if event.type == pygame.QUIT:
 					run = False
-		pygame.display.update() 
+			pygame.display.update() 
 
 # 
 game = Game()
--- scripts/settings.py
+++ scripts/settings.py
@@ -1,20 +1,67 @@
-
-# setup game window configuration variables
+# -- setup game window configuration variables --
 w = 1000; # screen width
-h = 1000; # screen height
+h = 900; # screen height
 
-# game variables
-tile_size = 50
+# -- game variables -- 
+tile_size = 100
 
 
-# images
+# -- world tile data --
 
-# world tile data
+# 9 x 10
+data = [
+	[1,1,1,1,1,1,1,1,1,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,0,1],
+	[1,1,1,1,1,1,1,1,1,1]
+]
 
-world = [
-	[1,1,1,1,1],
-	[1,0,0,0,1],
-	[1,0,0,0,1],
-	[1,0,0,0,1],
-	[1,1,1,1,1]
+# image tile locations
+imgT = [
+	'../assets/Tiles/bg_1.png',
+	'../assets/Tiles/sun.png',
+	'../assets/Tiles/bg_2.png',
+	'../assets/Tiles/grass.png',
+	'../assets/Tiles/grassCenter.png',
+	'../assets/Tiles/grassCenter_rounded.png',
+	'../assets/Tiles/grassCliffLeft.png',
+	'../assets/Tiles/grassCliffLeftAlt.png',
+	'../assets/Tiles/grassCliffRight.png',
+	'../assets/Tiles/grassCliffRightAlt.png',
+	'../assets/Tiles/grassHalf.png',
+	'../assets/Tiles/grassHalfLeft.png',
+	'../assets/Tiles/grassHalfMid.png',
+	'../assets/Tiles/grassHalfRight.png',
+	'../assets/Tiles/grassHillLeft.png',
+	'../assets/Tiles/grassHillLeft2.png',
+	'../assets/Tiles/grassHillRight.png',
+	'../assets/Tiles/grassHillRight2.png',
+	'../assets/Tiles/grassLeft.png',
+	'../assets/Tiles/grassMid.png',
+	'../assets/Tiles/grassRight.png',
+	'../assets/Tiles/bridge.png',
+	'../assets/Tiles/bridgeLogs.png'
+]
+
+# image items locations
+imgI = [
+	'../assets/.png',
+	'../assets/.png',
+	'../assets/.png',
+	'../assets/.png',
+	'../assets/.png'
+]
+
+# image player locations
+imgP = [
+	'../assets/.png',
+	'../assets/.png',
+	'../assets/.png',
+	'../assets/.png',
+	'../assets/.png'
 ]
